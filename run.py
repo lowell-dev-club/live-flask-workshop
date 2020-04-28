9278d426541a812d21f4d8613a1db7a58b473e65
Savagecoder77 <savagecoder77@gmail.com>
2020-04-28 01:08:37 -0700

    add commits to run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+# Import Flask app object from project folder (For python this means from the __init__.py file)
 from project import app
 
+# Run flask app with debug on and listening on port 8000
 app.run(
     debug=True,
     port=8000
