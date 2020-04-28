8a508ecb246a19a1aad4e2586a723ee51a2cff9a
Savagecoder77 <savagecoder77@gmail.com>
2020-04-28 01:12:17 -0700

    add comments to views.py

--- project/views.py
+++ project/views.py
@@ -1,6 +1,11 @@
+# Import Flask app object from project folder (For python this means from the __init__.py file)
 from project import app
+
+# Import functions from flask package
 from flask import render_template
 
+# Create new route at '/'
 @app.route('/')
 def home():
+    # return the rendered version of file.html
     return render_template('file.html')
